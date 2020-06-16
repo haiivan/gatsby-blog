35f6aeb2f11cfcc4ca11e4b654d5c7f0f5d84179
Iván Villegas <haivan_cva@gmail.com>
2020-06-16 11:50:12 -0500

    created new gatsby project and added markdown compiler

---
+++ src/pages/page-3.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout';
+
+function ThirdPage() {
+    return (
+        <Layout>
+            <h1>Hello from page 3!</h1>
+        </Layout>
+    )
+}
+
+export default ThirdPage
